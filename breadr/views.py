01f57fb8394833ff4e91c0d7479ae7ce8fa8a68f
= <=>
2021-01-06 16:50:03 -0500

    add bootstrap add titles

--- breadr/views.py
+++ breadr/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'breadr/index.html')
-
-def err404(request, exception=None):
-    return HttpResponseNotFound('404.html')
+    return render(request, 'breadr/index.html', {
+        'year': datetime.strftime(datetime.now(), '%Y')
+    })
